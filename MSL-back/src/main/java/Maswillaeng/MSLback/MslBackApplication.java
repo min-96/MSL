d54500c509a186bf5d5a5b7cc57a105ee6b1caa5
이종민 <102534186+lamodadite@users.noreply.github.com>
2023-01-12 18:44:57 +0900

    initial commit

---
+++ MSL-back/src/main/java/Maswillaeng/MSLback/MslBackApplication.java
@@ -0,0 +1,13 @@
+package Maswillaeng.MSLback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MslBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MslBackApplication.class, args);
+		System.out.println("hello3");
+	}
+}
