3b2f0c011cd7e09a8382153a07eb6b07b103d2c4
이종민 <102534186+lamodadite@users.noreply.github.com>
2023-02-16 00:38:29 +0900

    feat(좋아요): 좋아요 취소 기능 구현 (게시물, 댓글)

--- MSL-back/src/main/java/Maswillaeng/MSLback/controller/LikeController.java
+++ MSL-back/src/main/java/Maswillaeng/MSLback/controller/LikeController.java
@@ -6,6 +6,7 @@ import Maswillaeng.MSLback.utils.auth.AuthCheck;
 import Maswillaeng.MSLback.utils.auth.UserContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class LikeController {
 
     @AuthCheck(role = AuthCheck.Role.USER)
     @PostMapping("/post-like/{postId}")
-    public ResponseEntity<?> savePostLike(@PathVariable Long postId) {
+    public ResponseEntity<Object> savePostLike(@PathVariable Long postId) {
         likeService.savePostLike(UserContext.userData.get().getUserId(), postId);
         return ResponseEntity.ok().body(ResponseDto.of(
                 "게시물 좋아요가 성공적으로 저장되었습니다"
@@ -27,10 +28,26 @@ public class LikeController {
 
     @AuthCheck(role = AuthCheck.Role.USER)
     @PostMapping("/comment-like/{commentId}")
-    public ResponseEntity<?> saveCommentLike(@PathVariable Long commentId) {
+    public ResponseEntity<Object> saveCommentLike(@PathVariable Long commentId) {
         likeService.saveCommentLike(UserContext.userData.get().getUserId(), commentId);
         return ResponseEntity.ok().body(ResponseDto.of(
                 "댓글 좋아요가 성공적으로 저장되었습니다"
+        ));
+    }
+
+    @DeleteMapping("/post-like/{postId}")
+    public ResponseEntity<Object> deletePostLike(@PathVariable Long postId) {
+        likeService.deletePostLike(UserContext.userData.get().getUserId(), postId);
+        return ResponseEntity.ok().body(ResponseDto.of(
+                "게시물 좋아요가 성공적으로 취소되었습니다"
+        ));
+    }
+
+    @DeleteMapping("/comment-like/{commentId}")
+    public ResponseEntity<Object> deleteCommentLike(@PathVariable Long commentId) {
+        likeService.deleteCommentLike(UserContext.userData.get().getUserId(), commentId);
+        return ResponseEntity.ok().body(ResponseDto.of(
+                "댓글 좋아요가 성공적으로 취소되었습니다"
         ));
     }
 }
--- MSL-back/src/main/java/Maswillaeng/MSLback/service/LikeService.java
+++ MSL-back/src/main/java/Maswillaeng/MSLback/service/LikeService.java
@@ -2,9 +2,13 @@ package Maswillaeng.MSLback.service;
 
 import Maswillaeng.MSLback.common.exception.EntityNotFoundException;
 import Maswillaeng.MSLback.domain.entity.*;
+import Maswillaeng.MSLback.domain.entity.key.CommentLikeId;
+import Maswillaeng.MSLback.domain.entity.key.PostLikeId;
 import Maswillaeng.MSLback.domain.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.xml.bind.ValidationException;
 
 @RequiredArgsConstructor
 @Service
@@ -18,7 +22,7 @@ public class LikeService {
     public void savePostLike(Long userId, Long postId) {
         User user = userRepository.findById(userId).get();
         Post post = postRepository.findById(postId).orElseThrow(
-                () -> new EntityNotFoundException("존재하지 않는 게시물입니다."));
+                () -> new EntityNotFoundException("존재하지 않는 게시물입니다"));
 
         if (postLikeRepository.existsByUserAndPost(user, post)) {
             throw new IllegalStateException("해당 게시물에 이미 좋아요를 눌렀습니다");
@@ -43,5 +47,25 @@ public class LikeService {
                 .user(user)
                 .comment(comment).build();
         commentLikeRepository.save(postLike);
+    }
+
+    public void deletePostLike(Long userId, Long postId) {
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new EntityNotFoundException("존재하지 않는 게시물입니다"));
+        if (userId.equals(post.getUser().getId())) {
+            postLikeRepository.deleteById(new PostLikeId(userId, postId));
+        } else {
+            throw new RuntimeException("접근 권한 없음");
+        }
+    }
+
+    public void deleteCommentLike(Long userId, Long commentId) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(
+                () -> new EntityNotFoundException("존재하지 않는 게시물입니다"));
+        if (userId.equals(comment.getUser().getId())) {
+            commentLikeRepository.deleteById(new CommentLikeId(userId, commentId));
+        } else {
+            throw new RuntimeException("접근 권한 없음");
+        }
     }
 }
