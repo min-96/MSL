fc068486986b8247310a3e07dc6e45587f5d2565
이종민 <102534186+lamodadite@users.noreply.github.com>
2023-03-15 16:57:55 +0900

    refactor : 마지막 메세지 시간 명칭 변경

--- MSL-back/src/main/java/Maswillaeng/MSLback/domain/repository/ChatRoomQueryRepository.java
+++ MSL-back/src/main/java/Maswillaeng/MSLback/domain/repository/ChatRoomQueryRepository.java
@@ -1,17 +1,18 @@
 package Maswillaeng.MSLback.domain.repository;
 
 
-import Maswillaeng.MSLback.domain.entity.Chat;
 import Maswillaeng.MSLback.domain.entity.ChatRoom;
-import Maswillaeng.MSLback.domain.entity.QChat;
 import Maswillaeng.MSLback.dto.common.ChatRoomResponseDto;
-import com.querydsl.core.types.*;
+import com.querydsl.core.types.Expression;
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static Maswillaeng.MSLback.domain.entity.QChat.chat;
@@ -35,6 +36,30 @@ public class ChatRoomQueryRepository extends QuerydslRepositorySupport {
                 .where(chat.chatRoom.eq(chatRoom),
                         chat.recipientId.eq(userId),
                         chat.state.eq(false));
+        JPQLQuery<String> lastMessageSubQuery = JPAExpressions.select(chat.content)
+                .from(chat)
+                .where(chat.chatRoom.eq(chatRoom)
+                        .and(chat.createdAt.eq(
+                                        JPAExpressions.select(
+                                                        chat.createdAt.max())
+                                                .from(chat)
+                                                .where(chat.chatRoom.eq(chatRoom))
+                                )
+                        ))
+                .orderBy(chat.createdAt.desc())
+                .limit(1);
+
+        JPQLQuery<LocalDateTime> lastMessageTimeSubQuery = JPAExpressions.select(chat.createdAt)
+                .from(chat)
+                .where(chat.chatRoom.eq(chatRoom)
+                        .and(chat.createdAt.eq(
+                                        JPAExpressions.select(
+                                                        chat.createdAt.max())
+                                                .from(chat)
+                                                .where(chat.chatRoom.eq(chatRoom))
+                                )
+                        )).orderBy(chat.createdAt.desc())
+                .limit(1);
 
         return queryFactory
                 .select(Projections.bean(ChatRoomResponseDto.class,
@@ -43,39 +68,10 @@ public class ChatRoomQueryRepository extends QuerydslRepositorySupport {
                         user.nickName,
                         user.userImage,
                         ExpressionUtils.as(unReadMsgCntSubQuery, "unReadMsgCnt"),
-                                ExpressionUtils.as(
-                                        JPAExpressions.select(chat.content)
-                                                .from(chat)
-                                                .where(chat.chatRoom.eq(chatRoom)
-                                                .and(chat.createdAt.eq(
-                                                                JPAExpressions.select(
-                                                                                chat.createdAt.max())
-                                                                        .from(chat)
-                                                                        .where(chat.chatRoom.eq(chatRoom))
-                                                        )
-                                                ))
-                                                .orderBy(chat.createdAt.desc())
-                                                .limit(1)
-                                                ,"lastMessage"),
-                                        ExpressionUtils.as(
-                                                JPAExpressions.select(chat.createdAt)
-                                                        .from(chat)
-                                                        .where(chat.chatRoom.eq(chatRoom)
-                                                        .and(chat.createdAt.eq(
-                                                                        JPAExpressions.select(
-                                                                                        chat.createdAt.max())
-                                                                                .from(chat)
-                                                                                .where(chat.chatRoom.eq(chatRoom))
-                                                                )
-                                                        )).orderBy(chat.createdAt.desc())
-                                                        .limit(1),
-                                                "lastMessageCreatedAt")
+                        ExpressionUtils.as(lastMessageSubQuery, "lastMessage"),
+                        ExpressionUtils.as(lastMessageTimeSubQuery, "lastMessageTime")
                 ))
                 .from(chatRoom)
-      //          .leftJoin(chat).on(chat.chatRoom.eq(chatRoom))
-//                .leftJoin(chat).on(chat.chatRoom.eq(chatRoom)
-//                        .and(chat.recipientId.eq(userId))
-//                        .and(chat.state.eq(false)))
                 .leftJoin(user).on(user.id.eq(chatRoom.owner.id)
                         .or(user.id.eq(chatRoom.invited.id))
                         .and(user.id.ne(userId)))
